718b7403e49dec02fd1d8962332d3ff896101bc5
LizzyStrawberry <111688576+LizzyStrawberry@users.noreply.github.com>
2025-08-23 12:52:59 +0300

    Updated Ad Mechanics

--- example_mods/data/paycheck-(legacy)/Stun Mechanic.lua
+++ example_mods/data/paycheck-(legacy)/Stun Mechanic.lua
@@ -1,128 +1,85 @@
-local randomTimerNumb = 0
-local randomImageNumb = 0
 local randomBitchCount = 0
 local LizKianaNaked = 0
 
 local kill = false
-local songStarted = false
-local pressedKey = false
-local timerCompleted = false
-local turnedTimerOn = false
 
-local Keys = {'Left', 'Right', 'Up', 'Down', 'Dodge','Attack'}
-local numRand = Keys[math.random(#Keys)]
+local keyArray = {'A','B','C','D','SPACE'}
+local key = nil
+local cheatsOn = false
+
+local colors = {
+	'03fc20',
+    '03fcc2',
+    '0318fc',
+    '9003fc',
+    'fc03d2',
+    'fc0303',
+    'fc7303',
+    'fcf403'
+}
 
 local ended = false
 
-local cheatsOn = false
-
 function onCreatePost()
 	if mechanics and not ended then
-		if difficulty >= 0 then		
-			randomBitchCount = math.random(1,10)
-			LizKianaNaked = math.random(1,15)
-		
-			--creating text and images
-			makeLuaText('WarningText', 'PRESS '..numRand..' TO REMOVE POP UP!', 900, 0, 650)
-			setTextAlignment('WarningText', 'LEFT')
-			setTextSize('WarningText', 40)
-			setObjectCamera('WarningText', 'other')
+		if difficulty >= 0 then
+			--setting the random numbers for the timer, images and secret counts lmao
 			
-			makeLuaSprite('popup', 'effects/popups/Old/popup-'..math.random(1,10), 0, 0)
-			setScrollFactor('popup', 0, 0)
-			setObjectCamera('popup', 'hud')
-			setObjectOrder('popup', getObjectOrder('scoreTxt') + 1)
-			setProperty('popup.alpha', 0)
-			addLuaSprite('popup', true)
+			randomBitchCount = getRandomInt(1, 10) -- For Secret Text n.1
+			LizKianaNaked = getRandomInt(1, 15) -- For Secret Text n.2
+			
+			--creating text and images
+			makeLuaText('keyWarn', 'PRESS TO REMOVE POP UP!', 900, 0, 650)
+			setTextAlignment('keyWarn', 'LEFT')
+			setTextSize('keyWarn', 40)
+			setObjectCamera('keyWarn', 'other')
+			setProperty('keyWarn.visible', false)
+				
+			makeLuaSprite('advert', 'effects/popups/New/popup-'..getRandomInt(1, 10), 0, 0)
+			setScrollFactor('advert', 0, 0)
+			setObjectCamera('advert', 'hud')
+			setObjectOrder('advert', getObjectOrder('scoreTxt') + 1)
+			setProperty('advert.visible', false)
+			addLuaSprite('advert', true)
+			
+			addLuaText('keyWarn', true)
 		end
 	end
 end
 
 function onSongStart()
 	if mechanics and not ended then
-		if difficulty == 1 then
-			randomTimerNumb = math.random(10,20)
-		end
-		if difficulty == 0 then
-			randomTimerNumb = math.random(20,25)
-		end
-		songStarted = true
+		setUpContents()
 	end
 end
 
 function noteMiss(id, direction, noteType, isSustainNote)
-	if timerCompleted and noteType == '' and mechanics then
-		setProperty('health', health - 0)
+	if mechanics and noteType == "" and kill then
+		setProperty('health', getHealth() - 0)
 	end
 end
 
-function onUpdate()
-if mechanics and not ended then
-	if difficulty >= 0 then
-		--measuring the hp
-		health = getProperty('health')
-		
-		if cheatsOn == false and (botPlay or getPropertyFromClass('ClientPrefs', 'autoCharm') == 1) then
-			cheatsOn = true;
-		end
-		
-		--setting and running the timer
-		if not timerCompleted and songStarted and not turnedTimerOn then
-			--debugPrint('Setting Timer Up! Random Number is: ' ..randomTimerNumb)
-		
-			runTimer('blackoutStart', randomTimerNumb)
-			
-			pressedKey = false
-			turnedTimerOn = true
-		end
-		--pressing the key selected to redo the mechanic
-		if not cheatsOn and (not pressedKey and timerCompleted and keyJustPressed(numRand)) then
-			kill = false
-			
-			setProperty('popup.alpha', 0)
-			loadGraphic('popup', 'effects/popups/Old/popup-'..getRandomInt(1,10))
-
-			setProperty('WarningText.alpha', 0)
-			playSound('clickText')
-		
-			setProperty('boyfriend.stunned', false)
-
-			if difficulty == 1 then
-				randomTimerNumb = math.random(10,20)
+function onUpdate(elapsed)
+	if mechanics and not ended then
+		if difficulty >= 0 then
+			if not cheatsOn and (botPlay or getPropertyFromClass('ClientPrefs', 'autoCharm') == 1) then
+				cheatsOn = true
 			end
-			if difficulty == 0 then
-				randomTimerNumb = math.random(20,25)
-			end
-		
 			
-			numRand = Keys[math.random(#Keys)]
-		
-			timerCompleted = false
-			pressedKey = true
-			turnedTimerOn = false
-			drainHealth = false
-		end
-	
-		--setting text
-		if randomBitchCount == 7 then
-			setTextString('WarningText', 'TOUCH '..numRand..' IF YOU HAVE NO BITCHES')
-		elseif LizKianaNaked == 2 then
-			setTextString('WarningText', 'TOUCH '..numRand..' For Liz and Kiana Naked')
-		else
-			setTextString('WarningText', 'TOUCH '..numRand..' TO REMOVE POP UP!')
+			if kill and keyboardJustPressed(key) then
+				setUpContents()
+				playSound('clickText')
+			end
 		end
 	end
-end
 end
 
 function onStepHit()
-	if mechanics and not ended then
-		--text flash
-		if curStep % 4 == 0 and timerCompleted then
-			setProperty('WarningText.alpha', 1)
-		end
-		if curStep % 4 == 2 and timerCompleted then
-			setProperty('WarningText.alpha', 0)
+	if mechanics and not ended and kill then
+		if curStep % 4 == 0 then
+			setProperty('keyWarn.visible', true)
+		elseif curStep % 4 == 2 then
+			setProperty('keyWarn.visible', false)
 		end
 	end
 end
@@ -130,65 +87,65 @@ end
 function onTimerCompleted(tag)
 	if mechanics and not ended then
 		if difficulty >= 0 then
-		--once the timer is completed
-		if tag == 'blackoutStart' then
-			kill = true
-			timerCompleted = true
-			drainHealth = true
-			
-			setProperty('popup.alpha', 1)
-			addLuaText('WarningText', true)
-			
-			runTimer('killTimer', 3)
-
-			
-			playSound('ANGRY_TEXT_BOX')
-			
-			if cheatsOn then
-				runTimer('botPlayOn', 1)
+			if tag == 'popUpTimer' then
+				kill = true
+				playSound('ANGRY_TEXT_BOX')
+				
+				setProperty('advert.visible', true)
+				setProperty('boyfriend.stunned', true)
+				
+				local timeToKill = 5
+				if difficulty == 1 then timeToKill = 3 elseif difficulty == 2 then timeToKill = 1.5 end
+				runTimer('killTimer', timeToKill)
+				
+				if cheatsOn then runTimer('botPlayOn', 0.15) end
 			end
 			
-			setProperty('boyfriend.stunned', true)
-			
-			--debugPrint('Timer Works! Press Space to Reset the Timer!')
-			--debugPrint(timerCompleted)
-		end
-		if tag == 'botPlayOn' then
-			kill = false
-			
-			setProperty('popup.alpha', 0)
-			loadGraphic('popup', 'effects/popups/Old/popup-'..getRandomInt(1,10))
-			
-			setProperty('WarningText.alpha', 0)
-			playSound('clickText')
-			
-			setProperty('boyfriend.stunned', false)
-
-			if difficulty == 1 then
-				randomTimerNumb = math.random(10,20)
-			end
-			if difficulty == 0 then
-				randomTimerNumb = math.random(20,25)
+			if tag == 'botPlayOn' then
+				setUpContents()
+				playSound('clickText')
 			end
 			
-			numRand = Keys[math.random(#Keys)]
-			
-			timerCompleted = false
-			pressedKey = true
-			turnedTimerOn = false
-			drainHealth = false
-		end
-		if tag == 'killTimer' then
-			if kill == true then
+			if tag == 'killTimer' and kill then
 				setProperty('health', 0)
 			end
-		end
 		end
 	end
 end
 
+function setUpContents()
+	kill = false
+	setProperty('keyWarn.visible', false)
+	setProperty('advert.visible', false)
+				
+	-- Set Timer up
+	local minTime, maxTime = 20, 25
+	if difficulty == 1 then minTime = 10 maxTime = 15
+	elseif difficulty == 2 then minTime = 5 maxTime = 7 end
+	
+	local timeNum = getRandomInt(minTime, maxTime)
+	runTimer('popUpTimer', timeNum)
+	
+	-- Set up key to press + Text
+	key = keyArray[getRandomInt(1, #keyArray)]
+	
+	if randomBitchCount == 7 then setTextString('keyWarn', 'PRESS '..string.upper(key)..' IF YOU HAVE NO BITCHES')
+	elseif LizKianaNaked == 2 then setTextString('keyWarn', 'PRESS '..string.upper(key)..' For Liz and Kiana Naked') 
+	else setTextString('keyWarn', 'PRESS '..string.upper(key)..' TO REMOVE POP UP!')
+	end
+	
+	-- Set up BG Color + PopUp
+	local color = getColorFromHex(colors[getRandomInt(1, #colors)])
+	setProperty('colorChangingBG.color', color)
+	loadGraphic('advert', 'effects/popups/Old/popup-'..getRandomInt(1, 10))
+	
+	-- De-stun Player
+	setProperty('boyfriend.stunned', false)
+end
+
 function onEndSong()
 	ended = true
-	cancelTimer('blackoutStart')
+	cancelTimer('popUpTimer')
+	cancelTimer('killTimer')
 	return Function_Continue
 end
--- example_mods/data/paycheck/Stun Mechanic.lua
+++ example_mods/data/paycheck/Stun Mechanic.lua
@@ -1,20 +1,11 @@
-local randomTimerNumb = 0
-local randomImageNumb = 0
 local randomBitchCount = 0
 local LizKianaNaked = 0
 
 local kill = false
-local songStarted = false
-local pressedKey = false
-local timerCompleted = false
-local turnedTimerOn = false
 
-local Keys = {'Left', 'Right', 'Up', 'Down', 'Dodge', 'Attack'}
-local numRand = Keys[math.random(#Keys)]
-
-local ended = false
-
-local cheatsOn = false;
+local keyArray = {'A','B','C','D','SPACE'}
+local key = nil
+local cheatsOn = false
 
 local colors = {
 	'03fc20',
@@ -27,134 +18,75 @@ local colors = {
     'fcf403'
 }
 
+local ended = false
+
 function onCreatePost()
 	if mechanics and not ended then
 		if difficulty >= 0 then
 			--setting the random numbers for the timer, images and secret counts lmao
 			
-			randomBitchCount = math.random(1,10) -- For Secret Text n.1
-			LizKianaNaked = math.random(1,15) -- For Secret Text n.2
+			randomBitchCount = getRandomInt(1, 10) -- For Secret Text n.1
+			LizKianaNaked = getRandomInt(1, 15) -- For Secret Text n.2
 			
 			--creating text and images
-			makeLuaText('WarningText', 'TAP TO REMOVE POP UP!', 900, 0, 650)
-			setTextAlignment('WarningText', 'LEFT')
-			setTextSize('WarningText', 40)
-			setObjectCamera('WarningText', 'other')
+			makeLuaText('keyWarn', 'PRESS TO REMOVE POP UP!', 900, 0, 650)
+			setTextAlignment('keyWarn', 'LEFT')
+			setTextSize('keyWarn', 40)
+			setObjectCamera('keyWarn', 'other')
+			setProperty('keyWarn.visible', false)
 		
 			makeLuaSprite('colorChangingBG', 'effects/popups/New/colorChangingBG', 0, 0)
 			setScrollFactor('colorChangingBG', 0, 0)
 			setObjectCamera('colorChangingBG', 'hud')
-			setProperty('colorChangingBG.alpha', 0)
+			setProperty('colorChangingBG.visible', false)
 			setObjectOrder('colorChangingBG', getObjectOrder('scoreTxt') + 1)
 			addLuaSprite('colorChangingBG', true)
 				
-			makeLuaSprite('popup', 'effects/popups/New/popup-'..getRandomInt(1,10), 0, 0)
-			setScrollFactor('popup', 0, 0)
-			setObjectCamera('popup', 'hud')
-			setObjectOrder('popup', getObjectOrder('colorChangingBG') + 1)
-			setProperty('popup.alpha', 0)
-			addLuaSprite('popup', true)
+			makeLuaSprite('advert', 'effects/popups/New/popup-'..getRandomInt(1, 10), 0, 0)
+			setScrollFactor('advert', 0, 0)
+			setObjectCamera('advert', 'hud')
+			setObjectOrder('advert', getObjectOrder('colorChangingBG') + 1)
+			setProperty('advert.visible', false)
+			addLuaSprite('advert', true)
+			
+			addLuaText('keyWarn', true)
 		end
 	end
 end
 
 function onSongStart()
 	if mechanics and not ended then
-		if difficulty == 2 then
-			randomTimerNumb = math.random(5,7)
-		end
-		
-		if difficulty == 1 then
-			randomTimerNumb = math.random(10,15)
-		end
-		
-		if difficulty == 0 then
-			randomTimerNumb = math.random(20,25)
-		end
-		songStarted = true
+		setUpContents()
 	end
 end
 
 function noteMiss(id, direction, noteType, isSustainNote)
-	if timerCompleted and noteType == '' and mechanics then
-		setProperty('health', health - 0)
+	if mechanics and noteType == "" and kill then
+		setProperty('health', getHealth() - 0)
 	end
 end
 
-function onUpdate()
+function onUpdate(elapsed)
 	if mechanics and not ended then
 		if difficulty >= 0 then
-			health = getProperty('health')
-			
-			if cheatsOn == false and (botPlay or getPropertyFromClass('ClientPrefs', 'autoCharm') == 1) then
-				cheatsOn = true;
-			end
-		
-			--setting and running the timer
-			if not timerCompleted and songStarted and not turnedTimerOn then
-				--debugPrint('Setting Timer Up! Random Number is: ' ..randomTimerNumb)
-				
-				setProperty('colorChangingBG.color', getColorFromHex(colors[math.random(1, #(colors))]))
-				
-				runTimer('blackoutStart', randomTimerNumb)
-				
-				pressedKey = false
-				turnedTimerOn = true
+			if not cheatsOn and (botPlay or getPropertyFromClass('ClientPrefs', 'autoCharm') == 1) then
+				cheatsOn = true
 			end
 			
-			--pressing the key selected to redo the mechanic
-			if not pressedKey and timerCompleted and keyJustPressed(numRand) then
-				kill = false
-				
-				setProperty('popup.alpha', 0)
-				loadGraphic('popup', 'effects/popups/New/popup-'..getRandomInt(1,10))
-				
-				setProperty('colorChangingBG.alpha', 0)
-				setProperty('WarningText.alpha', 0)
+			if kill and keyboardJustPressed(key) then
+				setUpContents()
 				playSound('clickText')
-			
-				setProperty('boyfriend.stunned', false)
-			
-				if difficulty == 2 then
-					randomTimerNumb = math.random(5,7)
-				end
-				if difficulty == 1 then
-					randomTimerNumb = math.random(10,15)
-				end
-				if difficulty == 0 then
-					randomTimerNumb = math.random(20,25)
-				end
-			
-				numRand = Keys[math.random(#Keys)]
-			
-				timerCompleted = false
-				pressedKey = true
-				turnedTimerOn = false
-				drainHealth = false
-			end
-		
-			--setting text
-			if timerCompleted then
-				if randomBitchCount == 7 then
-					setTextString('WarningText', 'TOUCH '..numRand..' IF YOU HAVE NO BITCHES')
-				elseif LizKianaNaked == 2 then
-					setTextString('WarningText', 'TOUCH '..numRand..' For Liz and Kiana Naked')
-				else
-					setTextString('WarningText', 'TOUCH '..numRand..' TO REMOVE POP UP!')
-				end
 			end
 		end
 	end
 end
 
 function onStepHit()
-	if mechanics and not ended then
-		--text flash
-		if curStep % 4 == 0 and timerCompleted then
-			setProperty('WarningText.alpha', 1)
-		end
-		if curStep % 4 == 2 and timerCompleted then
-			setProperty('WarningText.alpha', 0)
+	if mechanics and not ended and kill then
+		if curStep % 4 == 0 then
+			setProperty('keyWarn.visible', true)
+		elseif curStep % 4 == 2 then
+			setProperty('keyWarn.visible', false)
 		end
 	end
 end
@@ -162,73 +94,67 @@ end
 function onTimerCompleted(tag)
 	if mechanics and not ended then
 		if difficulty >= 0 then
-		--once the timer is completed
-			if tag == 'blackoutStart' then
+			if tag == 'popUpTimer' then
 				kill = true
-				timerCompleted = true
-				drainHealth = true
-				
-				setProperty('colorChangingBG.alpha', 1)
-				setProperty('popup.alpha', 1)
-				addLuaText('WarningText', true)
-				
-				if difficulty == 2 then
-					runTimer('killTimer', 2)
-				else
-					runTimer('killTimer', 3)
-				end
-				
 				playSound('ANGRY_TEXT_BOX')
 				
-				if cheatsOn then
-					runTimer('botPlayOn', 0.3)
-				end
-				
+				setProperty('colorChangingBG.visible', true)
+				setProperty('advert.visible', true)
 				setProperty('boyfriend.stunned', true)
 				
-				--debugPrint('Timer Works! Press Space to Reset the Timer!')
-				--debugPrint(timerCompleted)
+				local timeToKill = 5
+				if difficulty == 1 then timeToKill = 3 elseif difficulty == 2 then timeToKill = 1.5 end
+				runTimer('killTimer', timeToKill)
+				
+				if cheatsOn then runTimer('botPlayOn', 0.15) end
 			end
+			
 			if tag == 'botPlayOn' then
-				kill = false
-				
-				setProperty('popup.alpha', 0)
-				loadGraphic('popup', 'effects/popups/New/popup-'..getRandomInt(1,10))
-				
-				setProperty('colorChangingBG.alpha', 0)
-				setProperty('WarningText.alpha', 0)
+				setUpContents()
 				playSound('clickText')
-			
-				setProperty('boyfriend.stunned', false)
-			
-				if difficulty == 2 then
-					randomTimerNumb = math.random(5,7)
-				end
-				if difficulty == 1 then
-					randomTimerNumb = math.random(10,15)
-				end
-				if difficulty == 0 then
-					randomTimerNumb = math.random(20,25)
-				end
-				
-				numRand = Keys[math.random(#Keys)]
-				
-				timerCompleted = false
-				pressedKey = true
-				turnedTimerOn = false
-				drainHealth = false
 			end
-			if tag == 'killTimer' then
-				if kill == true then
-					setProperty('health', 0)
-				end
+			
+			if tag == 'killTimer' and kill then
+				setProperty('health', 0)
 			end
 		end
 	end
 end
 
+function setUpContents()
+	kill = false
+	setProperty('colorChangingBG.visible', false)
+	setProperty('keyWarn.visible', false)
+	setProperty('advert.visible', false)
+				
+	-- Set Timer up
+	local minTime, maxTime = 20, 25
+	if difficulty == 1 then minTime = 10 maxTime = 15 
+	elseif difficulty == 2 then minTime = 5 maxTime = 7 end
+	
+	local timeNum = getRandomInt(minTime, maxTime)
+	runTimer('popUpTimer', timeNum)
+	
+	-- Set up key to press + Text
+	key = keyArray[getRandomInt(1, #keyArray)]
+	
+	if randomBitchCount == 7 then setTextString('keyWarn', 'PRESS '..string.upper(key)..' IF YOU HAVE NO BITCHES')
+	elseif LizKianaNaked == 2 then setTextString('keyWarn', 'PRESS '..string.upper(key)..' For Liz and Kiana Naked') 
+	else setTextString('keyWarn', 'PRESS '..string.upper(key)..' TO REMOVE POP UP!')
+	end
+	
+	-- Set up BG Color + PopUp
+	local color = getColorFromHex(colors[getRandomInt(1, #colors)])
+	setProperty('colorChangingBG.color', color)
+	loadGraphic('advert', 'effects/popups/New/popup-'..getRandomInt(1, 10))
+	
+	-- De-stun Player
+	setProperty('boyfriend.stunned', false)
+end
+
 function onEndSong()
 	ended = true
-	cancelTimer('blackoutStart')
+	cancelTimer('popUpTimer')
+	cancelTimer('killTimer')
 	return Function_Continue
 end
