4b532bb8b471c0de8fcb4f1887cff938e6377789
LizzyStrawberry <111688576+LizzyStrawberry@users.noreply.github.com>
2025-09-13 12:12:27 +0300

    Freeplay Categories Optimized
    - Cleaned up Freeplay Category States
    - Patched an issue in Villain In Board

--- example_mods/data/villain-in-board/Events.lua
+++ example_mods/data/villain-in-board/Events.lua
@@ -1,9 +1,9 @@
 function onCreate()
-	makeLuaSprite('sabotage', '', -800, -600)
-	makeGraphic('sabotage', 5000, 5000, 'FF0000')
+	makeLuaSprite('sabotage', nil, 0, 0)
+	makeGraphic('sabotage', screenWidth, screenHeight, 'FF0000')
 	setScrollFactor('sabotage', 0, 0)
-	setProperty('sabotage.alpha', 0)
-	setObjectCamera('sabotage', 'game')
+	setProperty('sabotage.alpha', 0.0001) -- Avoid lag
+	setObjectCamera('sabotage', 'hud')
 	addLuaSprite('sabotage', true)
 	
 	runTimer('bfDiesTime', getRandomInt(24, 72))
@@ -17,23 +17,21 @@ end
 
 function onBeatHit()
 	if curBeat % 4 == 0 and (curBeat >= 80 and curBeat <= 108) then
-		doTweenAlpha('sabotage', 'sabotage', 0.8, 0.06, 'circOut')
+		setProperty("sabotage.alpha", 0.8)
+		cancelTween("sabotageEnd")
+		doTweenAlpha('sabotageEnd', 'sabotage', 0, 0.7 / playbackRate, 'easeOut')
 	end
 	
 	if curBeat % 4 == 2 and (curBeat >= 109 and curBeat <= 142) then
-		doTweenAlpha('sabotage', 'sabotage', 0.8, 0.06, 'circOut')
-	end
-end
-
-function onTweenCompleted(tag)
-	if tag == 'sabotage' then
-		doTweenAlpha('sabotageEnd', 'sabotage', 0, 0.7, 'easeOut')
+		setProperty("sabotage.alpha", 0.8)
+		cancelTween("sabotageEnd")
+		doTweenAlpha('sabotageEnd', 'sabotage', 0, 0.7 / playbackRate, 'easeOut')
 	end
 end
 
 function onTimerCompleted(tag)
 	if tag == 'bfDiesTime' then
-		doTweenX('bfgoesweee', 'beef', 1840, 40, 'linear')
-		doTweenAngle('bfgoeswooo', 'beef', 360, 46, 'linear')
+		doTweenX('bfgoesweee', 'beef', 1840, 40 / playbackRate, 'linear')
+		doTweenAngle('bfgoeswooo', 'beef', 360, 46 / playbackRate, 'linear')
 	end
 end
--- source/FreeplayCategoryXtraState.hx
+++ source/FreeplayCategoryXtraState.hx
@@ -14,56 +14,47 @@ class FreeplayCategoryXtraState extends MusicBeatState
     var textBG:FlxSprite;
     var Text:FlxText;
 
-    var borders:FlxSprite;
-    var xtraShop:FlxSprite;
-    var xtraCrossover:FlxSprite;
-    var xtraBonus:FlxSprite;
-    var bgGradient:FlxSprite;
+    var selectionArray:Array<Array<Dynamic>> = [ // X, Y, type, isUnlocked
+        [135, 45, "Shop", true],
+        [480, 45, "Crossover", ClientPrefs.crossoverUnlocked],
+        [825, 45, "Bonus", ClientPrefs.xtraBonusUnlocked]
+    ];
+    var selectionGroup:FlxTypedGroup<FlxSprite>;
 
     override function create()
     {
-        #if desktop
+        FreeplayState.songCategory = '';
+
+        #if DISCORD_ALLOWED
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("Selecting XTRA Freeplay Mode", null);
 		#end
 
-        FreeplayState.songCategory = '';
-
-        bgGradient = new FlxSprite(0, 0).loadGraphic(Paths.image('pauseGradient/gradient_Null'));
+        var bgGradient:FlxSprite = new FlxSprite().loadGraphic(Paths.image('pauseGradient/gradient_Null'));
         bgGradient.color = 0xFF00a800;
-        bgGradient.x = 100;
-		bgGradient.scale.x = 1.3;
-		bgGradient.scale.y = 1.1;
+        bgGradient.setGraphicSize(FlxG.width, FlxG.height);
+        bgGradient.screenCenter(X);
 		bgGradient.alpha = 0;
-		bgGradient.scrollFactor.set();
 		add(bgGradient);
+        FlxTween.tween(bgGradient, {alpha: 1}, 3.4, {ease: FlxEase.quartInOut, type: PINGPONG});
         
-        xtraShop = new FlxSprite().loadGraphic(Paths.image('freeplayStuff/categories/XtraShop'));
-        xtraShop.x += 135;
-        xtraShop.y += 45;
-        xtraShop.antialiasing = ClientPrefs.globalAntialiasing;
-        xtraShop.updateHitbox();
-		add(xtraShop);
+        var assetLocation:String = "freeplayStuff/categories";
 
-        if (ClientPrefs.crossoverUnlocked == false)
-            xtraCrossover = new FlxSprite().loadGraphic(Paths.image('freeplayStuff/categories/XtraCrossoverLocked'));
-        else if (ClientPrefs.crossoverUnlocked == true)
-            xtraCrossover = new FlxSprite().loadGraphic(Paths.image('freeplayStuff/categories/XtraCrossover'));
-        xtraCrossover.x += 480;
-        xtraCrossover.y += 45;
-        xtraCrossover.antialiasing = ClientPrefs.globalAntialiasing;
-        xtraCrossover.updateHitbox();
-		add(xtraCrossover);
+        selectionGroup = new FlxTypedGroup<FlxSprite>();
+        add(selectionGroup);
+        for (i in 0...selectionArray.length)
+        {
+            var x = selectionArray[i][0]; var y = selectionArray[i][1];
+            var typeOfSel = selectionArray[i][2]; var isUnlocked = selectionArray[i][3];
 
-        if (ClientPrefs.xtraBonusUnlocked == false)
-            xtraBonus = new FlxSprite().loadGraphic(Paths.image('freeplayStuff/categories/XtraBonusLocked'));
-        else
-            xtraBonus = new FlxSprite().loadGraphic(Paths.image('freeplayStuff/categories/XtraBonus'));
-        xtraBonus.x += 825;
-        xtraBonus.y += 45;
-        xtraBonus.antialiasing = ClientPrefs.globalAntialiasing;
-        xtraBonus.updateHitbox();
-		add(xtraBonus);
+            var selection = new FlxSprite(x, y).loadGraphic(Paths.image('$assetLocation/Xtra$typeOfSel' + ((!isUnlocked) ? "Locked" : "")));
+            selection.ID = i;
+            selection.updateHitbox();
+            selection.antialiasing = ClientPrefs.globalAntialiasing;
+            selectionGroup.add(selection);
+
+            FlxTween.tween(selection, {y: selection.y + 5}, 5 + (i * 0.025), {ease: FlxEase.cubeInOut, type: PINGPONG});
+        }
 
         textBG = new FlxSprite(0, FlxG.height - 38).makeGraphic(FlxG.width, 46, 0xFF000000);
 		textBG.alpha = 0.6;
@@ -75,21 +66,18 @@ class FreeplayCategoryXtraState extends MusicBeatState
 		add(Text);
 
         FlxTween.tween(Text, {x: textBG.x - 2500}, 20, {ease: FlxEase.linear, type: LOOPING});
-        FlxTween.tween(bgGradient, {alpha: 1}, 3.4, {ease: FlxEase.quartInOut, type: PINGPONG});
-        FlxTween.tween(xtraShop, {y: xtraShop.y + 10}, 5, {ease: FlxEase.cubeInOut, type: PINGPONG});
-        FlxTween.tween(xtraCrossover, {y: xtraCrossover.y + 10}, 5.03, {ease: FlxEase.cubeInOut, type: PINGPONG});
-        FlxTween.tween(xtraBonus, {y: xtraBonus.y + 10}, 5.06, {ease: FlxEase.cubeInOut, type: PINGPONG});
 
         borders = new FlxSprite(0, 0).loadGraphic(Paths.image('promotion/Borders'));
+        borders.setGraphicSize(FlxG.width, FlxG.height);
 		borders.antialiasing = ClientPrefs.globalAntialiasing;
 		add(borders);
 
-        changeItem();
+        changeItem(0, false);
 
         super.create();
     }
 
-    var sideSelected:String = '';
+    var mouseMode:Bool = false;
     var selectedSomething:Bool = false;
     override function update(elapsed:Float)
     {
@@ -97,111 +85,66 @@ class FreeplayCategoryXtraState extends MusicBeatState
         {
             if (controls.BACK || FlxG.mouse.justPressedRight)
 		    {
-			    //persistentUpdate = false;
+                selectedSomething = true;
 			    FlxG.sound.play(Paths.sound('cancelMenu'));
 			    MusicBeatState.switchState(new FreeplayCategoryState(), 'stickers');
 		    }
 
-            if (FlxG.keys.justPressed.LEFT || FlxG.keys.justPressed.A)
-		    {
-			    FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-			    changeItem(-1);	
-		    }
+            if (controls.UI_LEFT_P) changeItem(-1);	
+   		    if (controls.UI_RIGHT_P) changeItem(1);
+            mouseMode = (FlxG.mouse.justMoved) ? true : false;
 
-		    if (FlxG.keys.justPressed.RIGHT || FlxG.keys.justPressed.D)
-		    {
-		    	FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-		    	changeItem(1);
-		    }
-
-            if (FlxG.mouse.overlaps(xtraShop) && (sideSelected == '' || sideSelected == 'RIGHT' || sideSelected == 'MIDDLE'))
+            selectionGroup.forEach(function(spr:FlxSprite)
             {
-                FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-                sideSelected = 'LEFT';
-                categorySelected = 0;
-		    	changeItem();	
-            }
-            if (FlxG.mouse.overlaps(xtraCrossover) && (sideSelected == '' || sideSelected == 'LEFT' || sideSelected == 'RIGHT'))
-            {
-                FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-                sideSelected = 'MIDDLE';
-                categorySelected = 1;
-                changeItem();	
-            }
-            if (FlxG.mouse.overlaps(xtraBonus) && (sideSelected == '' || sideSelected == 'MIDDLE' || sideSelected == 'LEFT'))
-            {
-                FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
-                sideSelected = 'RIGHT';
-                categorySelected = 2;
-                changeItem();	
-            }
-
-            if (categorySelected == 0 && (controls.ACCEPT || (FlxG.mouse.overlaps(xtraShop) && FlxG.mouse.justPressed)))
-            {
-                FlxFlicker.flicker(xtraShop, 1.5, 0.04, false);
-                FlxTween.tween(xtraCrossover, {alpha: 0}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
-                FlxTween.tween(xtraBonus, {alpha: 0}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
-                FreeplayState.songCategory = 'XTRASHOP';
-
-                FlxG.sound.play(Paths.sound('confirmMenu'));
-
-                selectedSomething = true;
-
-                new FlxTimer().start(1, function(tmr:FlxTimer)
+                if (FlxG.mouse.overlaps(spr) && mouseMode)
                 {
-                    MusicBeatState.switchState(new FreeplayState());
-                });
-            }
+                    if (categorySelected != spr.ID) FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+                    categorySelected = spr.ID; 
+                    changeItem(0, false);
+                }
 
-            if (ClientPrefs.crossoverUnlocked == true && categorySelected == 1 && (controls.ACCEPT || (FlxG.mouse.overlaps(xtraCrossover) && FlxG.mouse.justPressed)))
-            {
-                FlxFlicker.flicker(xtraCrossover, 1.5, 0.04, false);
-                FlxTween.tween(xtraShop, {alpha: 0}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
-                FlxTween.tween(xtraBonus, {alpha: 0}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
-                FreeplayState.songCategory = 'XTRACROSSOVER';
-    
-                FlxG.sound.play(Paths.sound('confirmMenu'));
-    
-                selectedSomething = true;
-                
-                new FlxTimer().start(1, function(tmr:FlxTimer)
-                 {
-                    MusicBeatState.switchState(new FreeplayState());
-                 });
-            }
-            if (ClientPrefs.crossoverUnlocked == false && categorySelected == 1 && (controls.ACCEPT || (FlxG.mouse.overlaps(xtraCrossover) && FlxG.mouse.justPressed)))
-            {
-                FlxG.camera.shake(0.007, 0.2, null, false, FlxAxes.XY);
-				FlxG.sound.play(Paths.sound('accessDenied'));
-            }
-
-            if (ClientPrefs.xtraBonusUnlocked == true && categorySelected == 2 && (controls.ACCEPT || (FlxG.mouse.overlaps(xtraBonus) && FlxG.mouse.justPressed)))
-            {
-                FlxFlicker.flicker(xtraBonus, 1.5, 0.04, false);
-                FlxTween.tween(xtraShop, {alpha: 0}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
-                FlxTween.tween(xtraCrossover, {alpha: 0}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
-                FreeplayState.songCategory = 'XTRABONUS';
-    
-                FlxG.sound.play(Paths.sound('confirmMenu'));
-    
-                selectedSomething = true;
-                
-                new FlxTimer().start(1, function(tmr:FlxTimer)
-                 {
-                    MusicBeatState.switchState(new FreeplayState());
-                 });
-            }
-            if (ClientPrefs.xtraBonusUnlocked == false && categorySelected == 2 && (controls.ACCEPT || (FlxG.mouse.overlaps(xtraBonus) && FlxG.mouse.justPressed)))
-            {
-                FlxG.camera.shake(0.007, 0.2, null, false, FlxAxes.XY);
-				FlxG.sound.play(Paths.sound('accessDenied'));
-            }
+                var accepted = controls.ACCEPT || (FlxG.mouse.overlaps(spr) && FlxG.mouse.justPressed);
+                if (accepted && spr.ID == categorySelected)
+                {
+                    var selectionName = "XTRA" + selectionArray[spr.ID][2].toUpperCase();
+                    var isUnlocked = selectionArray[spr.ID][3];
+                    // If unlocked, then move on
+                    if (isUnlocked) acceptCategory(selectionGroup, selectionName);
+                    else
+                    {
+                        FlxG.camera.shake(0.007, 0.2, null, false, FlxAxes.XY);
+                        FlxG.sound.play(Paths.sound('accessDenied'));  
+                    }
+                }
+            });
         }
         super.update(elapsed);
     }
 
-    function changeItem(huh:Int = 0)
+    function acceptCategory(group:FlxTypedGroup<FlxSprite>, freeplaySave:String)
+    {
+        FlxG.sound.play(Paths.sound('confirmMenu'));
+        selectedSomething = true;
+
+        group.forEach(function(spr:FlxSprite)
+        {
+            var flash = spr.ID == categorySelected;
+
+            if (flash) FlxFlicker.flicker(spr, 1.5, 0.04, false);
+            else FlxTween.tween(spr, {alpha: 0}, 0.7 + (spr.ID * 0.005), {ease: FlxEase.circOut, type: PERSIST});
+
+            FreeplayState.songCategory = freeplaySave;
+        });
+
+        new FlxTimer().start(1, function(tmr:FlxTimer)
+        {
+            MusicBeatState.switchState(new FreeplayState());
+        });
+    }
+
+    function changeItem(huh:Int = 0, ?playSound:Bool = true)
 	{
+        if (playSound) FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
         categorySelected += huh;
 	
 		if (categorySelected > 2)
@@ -209,31 +152,10 @@ class FreeplayCategoryXtraState extends MusicBeatState
 		if (categorySelected < 0)
 			categorySelected = 2;
 
-        if (categorySelected == 0) //Shop
+        selectionGroup.forEach(function(spr:FlxSprite)
         {
-            FlxTween.tween(xtraShop, {alpha: 1}, 0.4, {ease: FlxEase.circOut, type: PERSIST});
-
-            FlxTween.tween(xtraCrossover, {alpha: 0.4}, 0.4, {ease: FlxEase.circOut, type: PERSIST});
-
-            FlxTween.tween(xtraBonus, {alpha: 0.4}, 0.4, {ease: FlxEase.circOut, type: PERSIST});
-        }
-        
-        if (categorySelected == 1) //Crossover
-        {
-            FlxTween.tween(xtraShop, {alpha: 0.4}, 0.4, {ease: FlxEase.circOut, type: PERSIST});
-
-            FlxTween.tween(xtraCrossover, {alpha: 1}, 0.4, {ease: FlxEase.circOut, type: PERSIST});
-
-            FlxTween.tween(xtraBonus, {alpha: 0.4}, 0.4, {ease: FlxEase.circOut, type: PERSIST});
-        }
-
-        if (categorySelected == 2) //Bonus
-        {
-            FlxTween.tween(xtraShop, {alpha: 0.4}, 0.4, {ease: FlxEase.circOut, type: PERSIST});
-
-            FlxTween.tween(xtraCrossover, {alpha: 0.4}, 0.4, {ease: FlxEase.circOut, type: PERSIST});
-
-            FlxTween.tween(xtraBonus, {alpha: 1}, 0.4, {ease: FlxEase.circOut, type: PERSIST});
-        }
+            FlxTween.cancelTweensOf(spr, ["alpha"]);
+            FlxTween.tween(spr, {alpha: (spr.ID == categorySelected) ? 1 : 0.5}, 0.3, {ease: FlxEase.circOut, type: PERSIST});
+        });
     }
 }
