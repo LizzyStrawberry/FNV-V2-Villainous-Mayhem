c98ac6a438feb9ad35ac72178f949ef04abcfe16
LizzyStrawberry <111688576+LizzyStrawberry@users.noreply.github.com>
2025-09-17 12:09:20 +0300

    Achievement Menu Fix

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -53,7 +53,7 @@ class AchievementsMenuState extends MusicBeatState
 			add(icon);
 		}
 
-		descText = new FlxText(150, 600, 980, "", 32);
+		descText = new FlxText(MobileUtil.fixX(150), 600, 980, "", 32);
 		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		descText.scrollFactor.set();
 		descText.borderSize = 2.4;
