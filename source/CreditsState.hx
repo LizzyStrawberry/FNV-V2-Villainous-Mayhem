d6d7afb970c4f4932c974fc2a42edd1ac969e83b
LizzyStrawberry <111688576+LizzyStrawberry@users.noreply.github.com>
2025-10-13 11:31:36 +0300

    Cleaned up Credits

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -100,17 +100,17 @@ class CreditsState extends MusicBeatState
 		FlxTween.tween(tipDown, {x: textBGDown.x - 4000}, 27, {ease: FlxEase.linear, type: LOOPING});
 
 		creditsStuff = [ //Name - Icon name - Description - Quote - Link
-			['StatureGuy',	'statureguy', '<G>Roles:<G>\n- Owner of FNV\n- Director\n- Artist (All Assets + Cutscenes)\n- Main Voice Actor', "'I love Aileen'", 'https://www.youtube.com/channel/UCZ6Dkr6tseJJacSSjTd9OgQ', '00FBFF'],
-			['Lizzy Strawberry', 'strawberry', '<G>Roles:<G>\n- Main Coder\n- Main Sprite Animator\n- Song Coverer\n- Charter\n- Chromatic Scaler', "'<G>FNV<G> is the best mod ever, I'm not biased I swear,,'", 'https://www.youtube.com/c/LizzyStrawberry', 'f760eb'],
-			['Lillie', 'lillie', '<G>Roles:<G>\n- Additional Coder\n- Sprite Animator\n- Core Musician\n- Chromatic Creator\n- Charter ', "'<P>Oh? Did I<P> <G>Win<G>?'", 'https://www.youtube.com/@lilliancraig4690', 'fe8ca1'],
-			['TheRealOscamon', 'oscamon', '<G>Roles:<G>\n- Core Musician (Bangin fr)\n- Sprite Animator ', "'I am <GR>Oscamon<GR>..\nyes this is my quote'", 'https://www.youtube.com/channel/UC3kTxyzWnFopnfzLjD2jMuA', '18f55b'],
-			['HQC', 'hqc', '<G>Roles:<G>\n- Core Musician\n(Banger Music ong)', "'<C_>Kevin<C_>'", 'https://x.com/hqcs_', '6bebfb'],
-			['D3MON1X_', 'd3mon1x', '<G>Roles:<G>\n- Core Musician\n(Even more Banger Music fr)\n- Asul\'s Voice Actor', "'<R>Yaku<R> step on me plea-'", 'https://x.com/D3MON1X_', '843277'],
-			['Ricey', 'ricey', '<G>Roles:<G>\n- Former Core Musician\n(Banger Music once again)\n- Chromatic Creator ', "'uhm <R>i eat people for breakfast<R>'", 'https://twitter.com/hmhuhmh_r1', 'a548a3'],
-			['Shiloh', 'lanternAura', '<G>Roles:<G>\n- Core Musician\n(Coming in clutch fr)\n Chromatic Creator ', "'im <G>me<G>. me <G>be<G>. goddamn. <G>i am<G>'", 'https://lanternaura.carrd.co/', 'fc8901'],
-			['Araz', 'araz', '<G>Roles:<G>\n- Core Pause Menu Musician\n- Musician (Helped in Toxic Mishap (Legacy))', "'I forgot <G>how to music<G>'", 'https://www.youtube.com/channel/UCFefL4ngJkpsYFcBWlDC4RQ', 'FFF300'],
-			['Kitty', 'kitty', "<G>Roles:<G>\n- Narrin's Voice Actor", "<G>“Who the fuck is that”<G>\n— @K1ttyC0s3tt3", 'https://twitter.com/K1tty_C0s3tt3', 'f3327f'],
-			['AJTheFunky', 'aj', '<G>Roles:<G>\n- Main Charter\n- Made Hidden Menu Music Cover', "'Subscribe to <GR>AjTheFunky<GR> on <R>YouTube<R> :3'", 'https://ajthefunky.carrd.co/', '57ce78']
+			['StatureGuy',			'statureguy', 	'<G>Roles:<G>\n- Owner of FNV\n- Director\n- Artist (All Assets + Cutscenes)\n- Main Voice Actor', 			"'I love Aileen'", 												'https://www.youtube.com/channel/UCZ6Dkr6tseJJacSSjTd9OgQ', '00FBFF'],
+			['Lizzy Strawberry',	'strawberry', 	'<G>Roles:<G>\n- Main Coder\n- Main Sprite Animator\n- Song Coverer\n- Charter\n- Chromatic Scaler', 		"'<G>FNV<G> is the best mod ever, I'm not biased I swear,,'", 	'https://www.youtube.com/c/LizzyStrawberry', 				'f760eb'],
+			['Lillie', 				'lillie', 		'<G>Roles:<G>\n- Additional Coder\n- Sprite Animator\n- Core Musician\n- Chromatic Creator\n- Charter ', 	"'<P>Oh? Did I<P> <G>Win<G>?'", 								'https://www.youtube.com/@lilliancraig4690', 				'fe8ca1'],
+			['TheRealOscamon', 		'oscamon', 		'<G>Roles:<G>\n- Core Musician (Bangin fr)\n- Sprite Animator ', 											"'I am <GR>Oscamon<GR>..\nyes this is my quote'", 				'https://www.youtube.com/channel/UC3kTxyzWnFopnfzLjD2jMuA', '18f55b'],
+			['HQC', 				'hqc', 			'<G>Roles:<G>\n- Core Musician\n(Banger Music ong)', 														"'<C>Kevin<C>'", 												'https://x.com/hqcs_', 										'6bebfb'],
+			['D3MON1X_', 			'd3mon1x', 		'<G>Roles:<G>\n- Core Musician\n(Even more Banger Music fr)\n- Asul\'s Voice Actor', 						"'<R>Yaku<R> step on me plea-'", 								'https://x.com/D3MON1X_', 									'843277'],
+			['Ricey', 				'ricey', 		'<G>Roles:<G>\n- Former Core Musician\n(Banger Music once again)\n- Chromatic Creator ', 					"'Uhm <R>I eat people for breakfast<R>'", 						'https://twitter.com/hmhuhmh_r1', 							'a548a3'],
+			['Shiloh', 				'lanternAura', 	'<G>Roles:<G>\n- Core Musician\n(Coming in clutch fr)\n Chromatic Creator ', 								"'I'm <G>me<G>. me <G>be<G>. God damn. <G>I am.<G>'", 			'https://lanternaura.carrd.co/', 							'fc8901'],
+			['Araz', 				'araz', 		'<G>Roles:<G>\n- Core Pause Menu Musician\n- Musician (Helped in Toxic Mishap (Legacy))', 					"'I forgot <G>how to music<G>'", 								'https://www.youtube.com/channel/UCFefL4ngJkpsYFcBWlDC4RQ', 'FFF300'],
+			['Kitty', 				'kitty', 		"<G>Roles:<G>\n- Narrin's Voice Actor", 																	"<G>“Who the fuck is that”<G>\n— @K1ttyC0s3tt3", 				'https://twitter.com/K1tty_C0s3tt3', 						'f3327f'],
+			['AJTheFunky', 			'aj', 			'<G>Roles:<G>\n- Main Charter\n- Made Hidden Menu Music Cover', 											"'Subscribe to <GR>AjTheFunky<GR> on <R>YouTube<R> :3'", 		'https://ajthefunky.carrd.co/', 							'57ce78']
 		];
 
 		creditSprite = new FlxSprite(MobileUtil.fixX(-80)).loadGraphic(Paths.image('credits/FNVDevs/credits-' + creditsStuff[curSelected][1]));
@@ -186,13 +186,13 @@ class CreditsState extends MusicBeatState
 		// Former Section
 		formerCreditsStuff = [ //Name - Icon name - Description - Link - BG Color
 			['Former Developers'],
-			['Person Man',		'pman',		'Former Programmer (Le Funny Man)',								'https://www.youtube.com/channel/UCkvnsbNpey7uA8tG2dTDT9Q',	'A0A0A0'],
-			['JCD',			'jc',			"Former Sprite Animator (Old Marco + GF Sprites) \nI own Friday Night Huntin': Dimension Hunters BTW!",							'https://www.youtube.com/c/JCDelirious',		'FF362D'],
-			['Slash',				'slash',			"Former Chromatic Creator (Marco's Chromatic Scaler)",						'https://www.youtube.com/channel/UCpO66kLXQVIMS93aZ2lvZKA',			'3ED343'],
-			['Mayo Guy',			'mayoGuy',			'Former Charter',	'https://twitter.com/The_Mayo_Guy',			'929292'],
-			['Zuyu', 'zuyu', 'Former Charter / Musician', 'https://www.youtube.com/channel/UCPPZrEArPKQorpw7jrvOesw', '00FFFF'],
-			['Pixl', 'pixl', 'Former Charter', 'https://overpixel.carrd.co/', 'be43c0'],
-			['Rev', 'rev', 'Former Charter', 'https://revsstuffidk.carrd.co/', '5a7bff']
+			['Person Man',	'pman',		'Former Programmer (Le Funny Man)',																			'https://www.youtube.com/channel/UCkvnsbNpey7uA8tG2dTDT9Q',		'A0A0A0'],
+			['JCD',			'jc',		"Former Sprite Animator (Old Marco + GF Sprites) \nI own Friday Night Huntin': Dimension Hunters BTW!",		'https://www.youtube.com/c/JCDelirious',						'FF362D'],
+			['Slash',		'slash',	"Former Chromatic Creator (Marco's Chromatic Scaler)",														'https://www.youtube.com/channel/UCpO66kLXQVIMS93aZ2lvZKA',		'3ED343'],
+			['Mayo Guy',	'mayoGuy',	'Former Charter',																							'https://twitter.com/The_Mayo_Guy',								'929292'],
+			['Zuyu', 		'zuyu', 	'Former Charter / Musician', 																				'https://www.youtube.com/channel/UCPPZrEArPKQorpw7jrvOesw', 	'00FFFF'],
+			['Pixl', 		'pixl', 	'Former Charter', 																							'https://overpixel.carrd.co/', 									'be43c0'],
+			['Rev', 		'rev', 		'Former Charter', 																							'https://revsstuffidk.carrd.co/', 								'5a7bff']
 		];
 
 		grpOptions = new FlxTypedGroup<Alphabet>();
@@ -268,31 +268,22 @@ class CreditsState extends MusicBeatState
 		BGchecker.x += 0.5*(elapsed/(1/120));
         BGchecker.y += 0.36 / (ClientPrefs.framerate / 60); 
 
-		if (FlxG.sound.music.volume < 0.7)
-		{
-			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
-		}
+		if (FlxG.sound.music.volume < 0.7) FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
 
 		if(!quitting)
 		{
-			if (TouchUtil.overlaps(arrowSelectorLeft))
-				FlxTween.tween(arrowSelectorLeft, {x: getarrowSelectorLeftX - 2}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
-			else
-				FlxTween.tween(arrowSelectorLeft, {x: getarrowSelectorLeftX}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
+			if (TouchUtil.overlaps(arrowSelectorLeft)) FlxTween.tween(arrowSelectorLeft, {x: getarrowSelectorLeftX - 2}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
+			else FlxTween.tween(arrowSelectorLeft, {x: getarrowSelectorLeftX}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
 
-			if (TouchUtil.overlaps(arrowSelectorRight))
-				FlxTween.tween(arrowSelectorRight, {x: getarrowSelectorRightX + 2}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
-			else
-				FlxTween.tween(arrowSelectorRight, {x: getarrowSelectorRightX}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
+			if (TouchUtil.overlaps(arrowSelectorRight)) FlxTween.tween(arrowSelectorRight, {x: getarrowSelectorRightX + 2}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
+			else FlxTween.tween(arrowSelectorRight, {x: getarrowSelectorRightX}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
 
-			if (FlxG.keys.pressed.TAB || touchPad.buttonC.justPressed)
-				MusicBeatState.switchState(new LegacyCreditsState());	
+			if (FlxG.keys.pressed.TAB || touchPad.buttonC.justPressed) MusicBeatState.switchState(new LegacyCreditsState());	
 
 			if (controls.BACK)
 			{
-				if(colorTween != null) {
-					colorTween.cancel();
-				}
+				if(colorTween != null) colorTween.cancel();
+
 				FlxG.sound.play(Paths.sound('cancelMenu'));
 				MusicBeatState.switchState(new MainMenuState(), 'stickers');
 				quitting = true;
@@ -300,10 +291,8 @@ class CreditsState extends MusicBeatState
 
 			if (creditsSelected == 'CURRENT')
 			{
-				if(controls.UI_LEFT_P)
-					changeCredits(-1, true, true);
-				if(controls.UI_RIGHT_P)
-					changeCredits(1, true, true);
+				if(controls.UI_LEFT_P) changeCredits(-1, true, true);
+				if(controls.UI_RIGHT_P) changeCredits(1, true, true);
 
 				if (TouchUtil.overlaps(icon))
 				{
@@ -315,41 +304,25 @@ class CreditsState extends MusicBeatState
 					FlxTween.tween(icon, {y: getIconY}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
 					FlxTween.tween(icon, {alpha: 0.7}, 0.7, {ease: FlxEase.circOut, type: PERSIST});
 				}
-				if (TouchUtil.pressAction(icon) || controls.ACCEPT)
-					CoolUtil.browserLoad(creditsStuff[curSelected][4]);
+				if (TouchUtil.pressAction(icon) || controls.ACCEPT) CoolUtil.browserLoad(creditsStuff[curSelected][4]);
 			}
 			else if (creditsSelected == 'FORMER')
 			{
-				if(controls.UI_UP_P)
-					changeCredits(-1, true, true);
-				if(controls.UI_DOWN_P)
-					changeCredits(1, true, true);
+				if(controls.UI_UP_P) changeCredits(-1, true, true);
+				if(controls.UI_DOWN_P) changeCredits(1, true, true);
 
-				if (controls.ACCEPT || TouchUtil.pressAction(grpOptions.members[curSelectedFormer]))
-					CoolUtil.browserLoad(formerCreditsStuff[curSelectedFormer][3]);
+				if (controls.ACCEPT || TouchUtil.pressAction(grpOptions.members[curSelectedFormer])) CoolUtil.browserLoad(formerCreditsStuff[curSelectedFormer][3]);
 			}
 
 			if (!transitioning)
 			{
-				if (TouchUtil.pressAction(arrowSelectorLeft))
+				if (TouchUtil.pressAction(arrowSelectorLeft) || TouchUtil.pressAction(arrowSelectorRight))
 				{
-					if (creditsSelected == 'CURRENT')
-						changeCreditsCategory('FORMER');
-					else
-						changeCreditsCategory('CURRENT');
-					transitioning = true;
-				}
-		
-				if (TouchUtil.pressAction(arrowSelectorRight))
-				{
-					if (creditsSelected == 'CURRENT')
-						changeCreditsCategory('FORMER');
-					else
-						changeCreditsCategory('CURRENT');
+					if (creditsSelected == 'CURRENT') changeCreditsCategory('FORMER');
+					else changeCreditsCategory('CURRENT');
 					transitioning = true;
 				}
 			}
-			
 		}
 		
 		super.update(elapsed);
@@ -417,21 +390,15 @@ class CreditsState extends MusicBeatState
 			tipDown.text = "SWIPE: Change Developer | B: Go back to the Main Menu | Tap on DEV Icon to go to DEV's Social Media Page | LEFT / RIGHT ARROWS: Change Credits Category | C: Go to Special Credits";
 			curSelected += change;
 
-			if (curSelected < 0)
-				curSelected = 11;
-			if (curSelected > 11)
-				curSelected = 0;
+			if (curSelected < 0) curSelected = creditsStuff.length - 1;
+			if (curSelected > creditsStuff.length - 1) curSelected = 0;
 	
 			creditSprite.loadGraphic(Paths.image('credits/FNVDevs/credits-' + creditsStuff[curSelected][1]));
-			if (curSelected == 4)
-				creditSprite.scale.set(1.2, 1.2);
-			else
-				creditSprite.scale.set(1, 1);
+			if (curSelected == 4) creditSprite.scale.set(1.2, 1.2);
+			else creditSprite.scale.set(1, 1);
 
-			if (curSelected == 11)
-				creditSprite.x = MobileUtil.fixX(358.5);
-			else
-				creditSprite.x = MobileUtil.fixX(383.5);
+			if (curSelected == 11) creditSprite.x = MobileUtil.fixX(358.5);
+			else creditSprite.x = MobileUtil.fixX(383.5);
 	
 			icon.loadGraphic(Paths.image('credits/FNVDevIcons/' + creditsStuff[curSelected][1]));
 	
@@ -461,39 +428,8 @@ class CreditsState extends MusicBeatState
 		else if (creditsSelected == 'FORMER')
 		{
 			tipDown.text = "SWIPE: Change Credit | B: Go back to the Main Menu | Tap on DEV's name to go to DEV's Social Media Page | LEFT / RIGHT ARROWS: Change Credits Category | C: Go to Special Credits";
-		
-			curSelectedFormer += change;
-			if (curSelectedFormer < 1)
-				curSelectedFormer = 7;
-			if (curSelectedFormer > 7)
-				curSelectedFormer = 1;
-
-			var bullShit:Int = 0;
-			for (item in grpOptions.members)
-			{
-				item.targetY = bullShit - curSelectedFormer;
-				bullShit++;
-
-				if(!unselectableCheck(bullShit-1)) {
-					if (Math.abs(item.targetY) == 1)
-					{
-						FlxTween.tween(item, {alpha: 0.5}, 0.5, {ease: FlxEase.sineOut});
-						FlxTween.tween(item.scale, {x: 0.8, y: 0.8}, 0.5, {ease: FlxEase.sineOut});
-					}
-					if (Math.abs(item.targetY) >= 2)
-					{
-						FlxTween.tween(item, {alpha: 0}, 0.5, {ease: FlxEase.sineOut});
-						FlxTween.tween(item.scale, {x: 0.6, y: 0.6}, 0.5, {ease: FlxEase.sineOut});
-					}
-					if (item.targetY == 0)
-					{
-						FlxTween.tween(item, {alpha: 1}, 0.5, {ease: FlxEase.sineOut});
-						FlxTween.tween(item.scale, {x: 1, y: 1}, 0.5, {ease: FlxEase.sineOut});
-					}
-				}
-			}
-
-			descTextFormer.text = formerCreditsStuff[curSelectedFormer][2];
+			
+			fixFormerCredits(change);
 
 			if (tween)
 			{
@@ -507,9 +443,7 @@ class CreditsState extends MusicBeatState
 
 		var newColor:Int = getCurrentBGColor();
 		if(newColor != intendedColor) {
-			if(colorTween != null) {
-				colorTween.cancel();
-			}
+			if(colorTween != null) colorTween.cancel();
 			intendedColor = newColor;
 			colorTween = FlxTween.color(bg, 1, bg.color, intendedColor, {
 				onComplete: function(twn:FlxTween) {
@@ -521,18 +455,11 @@ class CreditsState extends MusicBeatState
 
 	function getCurrentBGColor() {
 		var bgColor:String;
-		if (creditsSelected == 'FORMER')
-			bgColor = formerCreditsStuff[curSelectedFormer][4];
-		else
-			bgColor = creditsStuff[curSelected][5];
-		if(!bgColor.startsWith('0x')) {
-			bgColor = '0xFF' + bgColor;
-		}
-		return Std.parseInt(bgColor);
-	}
+		if (creditsSelected == 'FORMER') bgColor = formerCreditsStuff[curSelectedFormer][4];
+		else bgColor = creditsStuff[curSelected][5];
 
-	private function unselectableCheck(num:Int):Bool {
-		return formerCreditsStuff[num].length <= 1;
+		if(!bgColor.startsWith('0x')) bgColor = '0xFF' + bgColor;
+		return Std.parseInt(bgColor);
 	}
 
 	var moveTween1:FlxTween = null;
@@ -542,44 +469,38 @@ class CreditsState extends MusicBeatState
 	var moveTweenScale2:FlxTween = null;
 	var moveTweenScale3:FlxTween = null;
 	function fixFormerCredits(change:Int = 0)
+	{
+		do 
 		{
 			curSelectedFormer += change;
-			if (curSelectedFormer < 1)
-				curSelectedFormer = 8;
-			if (curSelectedFormer > 8)
-				curSelectedFormer = 1;
+			if (curSelectedFormer < 0) curSelectedFormer = formerCreditsStuff.length - 1;
+			if (curSelectedFormer >= formerCreditsStuff.length) curSelectedFormer = 0;
+		} 
+		while(unselectableCheck(curSelectedFormer));
 	
-			var bullShit:Int = 0;
-			for (item in grpOptions.members)
-			{
-				item.targetY = bullShit - curSelectedFormer;
-				bullShit++;
+		var bullShit:Int = 0;
+		for (item in grpOptions.members)
+		{
+			item.targetY = bullShit - curSelectedFormer;
+			bullShit++;
 
-				if(!unselectableCheck(bullShit-1)) {
-					if (Math.abs(item.targetY) == 1)
-					{
-						if(moveTween2 != null) moveTween2.cancel();
-						if(moveTweenScale2 != null) moveTweenScale2.cancel();
-						moveTween2 = FlxTween.tween(item, {alpha: 0.5}, 0.5, {ease: FlxEase.sineOut});
-						moveTweenScale2 = FlxTween.tween(item.scale, {x: 0.8, y: 0.8}, 0.5, {ease: FlxEase.sineOut});
-					}
-					if (Math.abs(item.targetY) >= 2)
-					{
-						if(moveTween3 != null) moveTween3.cancel();
-						if(moveTweenScale3 != null) moveTweenScale3.cancel();
-						moveTween3 = FlxTween.tween(item, {alpha: 0}, 0.5, {ease: FlxEase.sineOut});
-						moveTweenScale3 = FlxTween.tween(item.scale, {x: 0.6, y: 0.6}, 0.5, {ease: FlxEase.sineOut});
-					}
-					if (item.targetY == 0)
-					{
-						if(moveTween1 != null) moveTween1.cancel();
-						if(moveTweenScale1 != null) moveTweenScale1.cancel();
-						moveTween1 = FlxTween.tween(item, {alpha: 1}, 0.5, {ease: FlxEase.sineOut});
-						moveTweenScale1 = FlxTween.tween(item.scale, {x: 1, y: 1}, 0.5, {ease: FlxEase.sineOut});
-					}
+			if(!unselectableCheck(bullShit - 1))
+			{
+				FlxTween.cancelTweensOf(item);
+				switch(Math.abs(item.targetY))
+				{
+					case 0: FlxTween.tween(item, {alpha: 1, "scale.x": 1, "scale.y": 1}, 0.5, {ease: FlxEase.sineOut});
+					case 1: FlxTween.tween(item, {alpha: 0.5, "scale.x": 0.75, "scale.y": 0.75}, 0.5, {ease: FlxEase.sineOut});
+					default: FlxTween.tween(item, {alpha: 0, "scale.x": 0.5, "scale.y": 0.5}, 0.5, {ease: FlxEase.sineOut});
 				}
 			}
 	
 			descTextFormer.text = formerCreditsStuff[curSelectedFormer][2];
 		}
+	}
+
+	private function unselectableCheck(num:Int):Bool
+	{
+		return formerCreditsStuff[num].length <= 1;
+	}
 }
