3eb7b663b5c2141ff15290ac8bcab379b8bd4a42
LizzyStrawberry <111688576+LizzyStrawberry@users.noreply.github.com>
2025-10-19 11:13:10 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -11,7 +11,7 @@ import flash.system.System;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var FNVVersion:String = '2.0'; //This is also used for Discord RPC
+	public static var FNVVersion:String = '2.0.1 (Hotfix)'; //This is also used for Discord RPC
 	public static var psychEngineVersion:String = '0.6.3 Modified'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 	public static var curStorySelected:Int = 0;
